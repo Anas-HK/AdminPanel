ff9c24c5f2b98d698b57eded88fa157a2513d152
Anas Khan <anas.h.k2244@gmail.com>
2024-03-17 19:52:08 +0500

    Routing successfull from dashboard to businenss

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\BusinessController;
 
 Route::group(['middleware' => 'auth'], function () {
     // Define routes that require authentication here
     Route::get('/admin_dashboard', [DashboardController::class, 'adminDashboard'])->name('admin.admin_dashboard');
+    Route::get('/businesses', [BusinessController::class, 'index'])->name('admin.businesses');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
